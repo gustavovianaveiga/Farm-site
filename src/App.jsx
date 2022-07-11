5c1c3be81ff2402cbb74eac3724e04948f827c96
gustavo viana <gustavovianaveiga721@gmail.com>
2022-07-11 14:25:57 -0300

    farmlogo adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-
-
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import './App.css'
+import Home from "./pages/home";
+import Contato from "./pages/contato";
 function App() {
 
 
+
   return (
-    <div>hello world</div>
+    <div className="App">
+
+      <header>
+        <section className="header">
+          <img src='/src/img/farm-logo.png' alt="farm-logo" className="farmlogo" />
+          {/* <Link to="/contato">contato</Link> */}
+        </section>
+      </header>
+
+      <main>
+        <section className='estrutura'>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </section>
+      </main>
+
+      <footer></footer>
+    </div>
   )
 }
 
