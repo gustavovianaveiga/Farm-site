3ad2f58063d980387235bef88fcaa8716ce49c0f
gustavo viana <gustavovianaveiga721@gmail.com>
2022-07-13 00:47:13 -0300

    botao fechar navbar funcionando

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 function Navbar() {
+    function botaox() {
+        const $navbar = document.querySelector(".navbar");
+        const $x = document.querySelector(".menux");
+        const $img = document.querySelector(".logonavbar");
+        const $links = document.querySelector(".links");
+        $x.style.marginLeft = '-50px';
+        $x.style.opacity = '0';
+        $img.style.marginLeft = '-50px';
+        $img.style.opacity = '0';
+        $links.style.marginLeft = '-100px';
+        $links.style.opacity = '0';
+        $navbar.style.width = "0px";
+    };
     return (
         <nav className='navbar'>
-            <img src="/src/img/farm-logo.png" alt="farmlogo" />
+            <img src="/src/img/farm-logo.png" alt="farmlogo" className='logonavbar' />
             <div className='links'>
                 <ul>
                     <Link to="/home">inicio</Link>
                     <Link to="/loja">loja</Link>
                     <Link to="/contato">contato</Link>
                     <Link to="/sobre">sobre</Link>
-                    <div className='menux'>
-                        <ul></ul>
-                    </div>
                 </ul>
+            </div>
+            <div className='menux' onClick={botaox}>
+                <ul></ul>
             </div>
         </nav>
 
